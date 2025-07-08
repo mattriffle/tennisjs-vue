24eaa9e09d59a63f251964cbb446b77c4410f460
Matt Riffle <matt@pair.com>
2025-07-07 21:06:41 -0400

    fix build errors

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+import Components from "unplugin-vue-components/vite";
+import { fileURLToPath, URL } from "node:url";
+import vue from "@vitejs/plugin-vue";
+// https://vite.dev/config/
+export default defineConfig({
+    plugins: [vue(), Components({})],
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
+        },
+    },
+});
